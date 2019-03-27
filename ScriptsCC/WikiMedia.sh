739495981ab8c8c87e888f45cf9f8d35bc9ed1cc
Manuel Parra <manuparra@gmail.com>
2019-03-27 12:05:23 +0100

    Update WikiMedia.sh

--- ScriptsCC/WikiMedia.sh
+++ ScriptsCC/WikiMedia.sh
@@ -6,9 +6,11 @@ sudo apt update
 sudo apt-cache policy docker-ce
 sudo apt install -y docker-ce
 sudo docker pull mediawiki
-sudo docker run --name=mediawiki_wiki -p 80:8080 -e MEDIAWIKI_SERVER=http://localhost:80 -e MEDIAWIKI_SITENAME=SistemaDocumental -e MEDIAWIKI_LANGUAGE_CODE=es  -e MEDIAWIKI_DB_TYPE=sqlite  -e MEDIAWIKI_DB_NAME=wikidb  -e MEDIAWIKI_ENABLE_UPLOADS=1  -v /var/mediawiki/images:/images  -v /var/mediawiki/data:/data -d kristophjunge/mediawiki
-sudo docker exec -it mediawiki_wiki /script/install.sh admin admin
+#sudo docker run --name=mediawiki_wiki -p 80:8080 -e MEDIAWIKI_SERVER=http://localhost:80 -e MEDIAWIKI_SITENAME=SistemaDocumental -e MEDIAWIKI_LANGUAGE_CODE=es  -e MEDIAWIKI_DB_TYPE=sqlite  -e MEDIAWIKI_DB_NAME=wikidb  -e MEDIAWIKI_ENABLE_UPLOADS=1  -v /var/mediawiki/images:/images  -v /var/mediawiki/data:/data -d kristophjunge/mediawiki
+#sudo docker exec -it mediawiki_wiki /script/install.sh admin admin
 sudo ufw allow from any to any port 80 proto tcp
+sudo docker run --name some-mediawiki -p 80:80 -d mediawiki
+
 
 
 
