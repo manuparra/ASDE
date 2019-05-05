1954c2c501e11aef82dcbe180a0268675ab67d23
Manuel Parra <manuparra@gmail.com>
2019-05-05 23:24:00 +0200

    Update sugarcrm.sh

--- ScriptsCC/sugarcrm.sh
+++ ScriptsCC/sugarcrm.sh
@@ -7,5 +7,6 @@ sudo apt install -y docker-ce
 sudo git clone https://github.com/Spantree/docker-sugarcrm
 sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+cd docker-sugarcrm
 sudo docker-compose up
 
