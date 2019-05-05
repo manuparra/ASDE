6aab44a1493895440246fcd205f432368e55715b
Manuel Parra <manuparra@gmail.com>
2019-05-05 23:15:36 +0200

    Update sugarcrm.sh

--- ScriptsCC/sugarcrm.sh
+++ ScriptsCC/sugarcrm.sh
@@ -4,3 +4,5 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 sudo apt update
 sudo apt-cache policy docker-ce
 sudo apt install -y docker-ce
+sudo git clone https://github.com/Spantree/docker-sugarcrm
+sudo docker-compose up
